968c8020d6ea5bdffec03102bbb77d10e60aef11
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-26 00:43:06 +0100

    Adds code to the list and returns the new created one

--- src/barcode/barcode.service.js
+++ src/barcode/barcode.service.js
@@ -4,3 +4,18 @@ let codeData = require('./barcode.data');
 
 const getCodes = () => Promise.resolve(codeData);
 
+const addCode = async (inputStr) => {
+  const newCode = {
+    id: v4(),
+  };
+
+  return new Promise((resolve, reject) => {
+    QRCode.toDataURL(inputStr, (err, url) => {
+      if (err) reject(err);
+
+      newCode.codeUrl = url;
+      codeData = [...codeData, newCode];
+      resolve(newCode);
+    });
+  });
+};
