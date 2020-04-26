6d506683a9de4e1c89a98f9843113fbd6f5a77d4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-26 01:13:47 +0100

    Added a validation for the dataString property in the req body

--- src/barcode/index.js
+++ src/barcode/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { check } = require('express-validator');
 const { addQRCode, getAllCodes } = require('./barcode.controller');
 
 const router = express.Router();
 
 router.get('/', getAllCodes);
-router.post('/', addQRCode);
+router.post('/', [check('dataString').isLength({ min: 3 })], addQRCode);
 
 module.exports = router;
