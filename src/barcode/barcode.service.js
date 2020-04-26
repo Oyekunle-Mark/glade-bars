3eaecbebcebffdd75dd74273e584f528849b42f0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-26 11:14:14 +0100

    Added a plain text title to the code objects

--- src/barcode/barcode.service.js
+++ src/barcode/barcode.service.js
@@ -7,13 +7,14 @@ const getCodes = () => Promise.resolve(codeData);
 const addCode = async (inputStr) => {
   const newCode = {
     id: v4(),
+    title: inputStr,
   };
 
   return new Promise((resolve, reject) => {
     QRCode.toDataURL(inputStr, (err, url) => {
       if (err) reject(err);
 
-      newCode.codeUrl = url;
+      newCode.code = url;
       codeData = [...codeData, newCode];
       resolve(newCode);
     });
